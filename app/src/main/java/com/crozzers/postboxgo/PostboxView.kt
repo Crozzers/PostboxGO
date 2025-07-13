19bf7d2fb2c818f0196dd1d635d7f8a45e3141e2
Crozzers <captaincrozzers@gmail.com>
2025-07-13 17:54:24 +0100

    Fix postbox details view in landscape

--- app/src/main/java/com/crozzers/postboxgo/PostboxView.kt
+++ app/src/main/java/com/crozzers/postboxgo/PostboxView.kt
@@ -1,6 +1,7 @@
 package com.crozzers.postboxgo
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.LocationOn
@@ -20,10 +23,16 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,87 +46,145 @@ import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun ViewPostbox(postbox: Postbox, saveFile: SaveFile, deleteCallback: () -> Unit) {
-    val postboxPos = LatLng(
-        postbox.coords.first.toDouble(),
-        postbox.coords.second.toDouble()
-    )
-    val cameraPosState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(
-            postboxPos, 15f
-        )
-    }
-    val context = LocalContext.current
-    val openConfirmDeleteDialog = remember { mutableStateOf(false) }
+    var orientation by remember { mutableIntStateOf(Configuration.ORIENTATION_PORTRAIT) }
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(text = postbox.name, fontSize = 24.sp)
-        Text(text = "Registered: ${postbox.dateRegistered}", fontSize = 12.sp, color = Color.Gray)
-        Text(text = "Type: ${postbox.type ?: "Unknown"}")
-        Text(text = "Monarch: ${postbox.monarch}")
-        Text(text = "Location: ${postbox.coords.first}, ${postbox.coords.second}")
-        GoogleMap(
-            modifier = Modifier
-                .height(500.dp),
-            cameraPositionState = cameraPosState,
-        ) {
-            Marker(
-                state = MarkerState(position = postboxPos),
-                title = "Postbox"
-            )
-        }
-        Spacer(Modifier.height(16.dp))
-        Button({
-            context.startActivity(
-                Intent(
-                    Intent.ACTION_VIEW,
-                    "https://maps.google.com/maps/dir//${postbox.coords.first},${postbox.coords.second}".toUri()
-                )
-            )
-        }, modifier = Modifier.fillMaxWidth()) {
-            Row() {
-                Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "Get Directions")
-                Text(text = "Get Directions")
-            }
-        }
-        Spacer(Modifier.height(16.dp))
-        OutlinedButton(
-            {
-                openConfirmDeleteDialog.value = true
-            },
-            modifier = Modifier
-                .fillMaxWidth(),
-            border = BorderStroke(1.dp, Color.Red),
-        ) {
-            Row() {
-                Icon(
-                    imageVector = Icons.Filled.Delete,
-                    contentDescription = "Delete Postbox",
-                    tint = Color.Red
-                )
-                Text(text = "Delete Postbox", color = Color.Red)
-            }
-        }
-        when {
-            openConfirmDeleteDialog.value -> {
-                ConfirmDelete(callback = { state ->
-                    openConfirmDeleteDialog.value = false
+    val configuration = LocalConfiguration.current
+    LaunchedEffect(configuration) {
+        snapshotFlow { configuration.orientation }
+            .collect { orientation = it }
+    }
+
+    when (orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                PostboxDetails(postbox)
+                PostboxMap(postbox.coords, Modifier.height(500.dp))
+                ActionButtons(
+                    postbox.coords
+                ) { state ->
                     if (state) {
                         saveFile.removePostbox(postbox)
                         deleteCallback()
                     }
+                }
+            }
+        }
 
-                })
+        else -> {
+            Row(
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth(0.5f)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    PostboxDetails(postbox)
+                    ActionButtons(
+                        postbox.coords
+                    ) { state ->
+                        if (state) {
+                            saveFile.removePostbox(postbox)
+                            deleteCallback()
+                        }
+                    }
+                }
+                Column(
+                    Modifier.fillMaxSize()
+                ) {
+                    PostboxMap(postbox.coords, Modifier.fillMaxSize())
+                }
             }
         }
     }
 }
 
 @Composable
-fun ConfirmDelete(callback: (state: Boolean) -> Unit) {
+fun PostboxDetails(postbox: Postbox) {
+    Text(text = postbox.name, fontSize = 24.sp)
+    Text(text = "Registered: ${postbox.dateRegistered}", fontSize = 12.sp, color = Color.Gray)
+    Text(text = "Type: ${postbox.type ?: "Unknown"}")
+    Text(text = "Monarch: ${postbox.monarch}")
+    Text(text = "Location: ${postbox.coords.first}, ${postbox.coords.second}")
+}
+
+@Composable
+fun PostboxMap(coords: Pair<Float, Float>, modifier: Modifier = Modifier) {
+    val postboxPos = LatLng(
+        coords.first.toDouble(),
+        coords.second.toDouble()
+    )
+    val cameraPosState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(
+            postboxPos, 15f
+        )
+    }
+    GoogleMap(
+        modifier = modifier,
+        cameraPositionState = cameraPosState,
+    ) {
+        Marker(
+            state = MarkerState(position = postboxPos),
+            title = "Postbox"
+        )
+    }
+}
+
+@Composable
+fun ActionButtons(coords: Pair<Float, Float>, deleteCallback: (s: Boolean) -> Unit) {
+    val context = LocalContext.current
+    val openConfirmDeleteDialog = remember { mutableStateOf(false) }
+
+    Spacer(Modifier.height(16.dp))
+    Button({
+        context.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                "https://maps.google.com/maps/dir//${coords.first},${coords.second}".toUri()
+            )
+        )
+    }, modifier = Modifier.fillMaxWidth()) {
+        Row() {
+            Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "Get Directions")
+            Text(text = "Get Directions")
+        }
+    }
+    Spacer(Modifier.height(16.dp))
+    OutlinedButton(
+        {
+            openConfirmDeleteDialog.value = true
+        },
+        modifier = Modifier
+            .fillMaxWidth(),
+        border = BorderStroke(1.dp, Color.Red),
+    ) {
+        Row() {
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = "Delete Postbox",
+                tint = Color.Red
+            )
+            Text(text = "Delete Postbox", color = Color.Red)
+        }
+    }
+    when {
+        openConfirmDeleteDialog.value -> {
+            ConfirmDeleteDialog(callback = { state ->
+                openConfirmDeleteDialog.value = false
+                deleteCallback(state)
+            })
+        }
+    }
+}
+
+@Composable
+fun ConfirmDeleteDialog(callback: (state: Boolean) -> Unit) {
     AlertDialog(
         icon = {
             Icon(
