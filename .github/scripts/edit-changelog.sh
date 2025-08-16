d3a693f03ee9a0218c810380c4e8c45e06820a35
Crozzers <captaincrozzers@gmail.com>
2025-08-16 15:48:47 +0100

    Add a release workflow

---
+++ .github/scripts/edit-changelog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+version="$1"
+TAG_PREFIX="$2"
+
+prevVersion=$(
+    cat CHANGELOG.md | \
+    # get lines with version nums on them
+    grep '## \[[0-9].*\] - [0-9]\{4\}\(-[0-9]\{2\}\)\{2\}' | \
+    # extract version number
+    grep -o '[0-9]\+\(\.[0-9]\+\)\{2\}' | \
+    # get latest one
+    head -n 1
+)
+
+# add new header for new version
+awk -v version="$version" -v date="$(date +'%Y-%m-%d')" '
+    1;/## \[Unreleased\]/{ print "\n\n## [" version "] - " date }
+' CHANGELOG.md > CHANGELOG.md.temp
+
+mv CHANGELOG.md.temp CHANGELOG.md
+
+# update links
+sed -ri 's|(\[unreleased\]: .*compare\/'$TAG_PREFIX')(.*)(\.\.\.HEAD)|\1'$version'\3|g' CHANGELOG.md
+
+awk -v prev="$TAG_PREFIX$prevVersion" \
+    -v prefix="$TAG_PREFIX" \
+    -v version="$version" \
+    -v repo="$GITHUB_REPOSITORY" '
+    1;/\[unreleased\]: /{
+        print "[" version "]: https://github.com/Crozzers/PostboxGO/compare/" prev "..." prefix version
+    }
+    ' CHANGELOG.md > CHANGELOG.md.temp
+
+mv CHANGELOG.md.temp CHANGELOG.md
+
+echo "new changelog:"
+echo "--------------"
+cat CHANGELOG.md
---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## [unreleased]
+
+- Initial release
+
+
+[unreleased]: https://github.com/Crozzers/PostboxGO/compare/a83a8e1...HEAD
